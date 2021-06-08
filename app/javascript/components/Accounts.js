fe7a461be04c30fd15588b1a1f068796435b597c
Henry Doan <henrydoan96@gmail.com>
2021-06-07 21:00:55 -0600

    added accounts

---
+++ app/javascript/components/Accounts.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Accounts = ({ accounts, user }) => {
+  const { first_name, last_name } = user
+  return (
+    <>
+      <h1>{first_name} {last_name}'s Accounts</h1>
+      <hr/>
+      <a href="/accounts/new">New Account</a>
+      <h3>{accounts.length < 0 ? "No Accounts" : "Your Accounts:"}</h3>
+      <ul>
+        {
+          accounts.map( (a) => (
+            <li key={a.id}>
+              {a.name} has a balance of: {a.balance}
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  )
+}
+
+export default Accounts;
---
+++ app/javascript/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return(
+    <nav>
+      <ul>
+        <li>
+          <a href="/users/sign_out" data-method="delete">
+            Logout
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
